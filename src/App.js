7ed8445d5840dc85eee7dfe22d5c475c4751dacd
Mcdonalds Trump <whdmschd2@gmail.com>
2021-12-22 20:56:21 +0900

    merge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import Nav from './components/Nav';
-import Home from './routes/Home';
-import Write from './routes/Write';
-import List from './routes/List';
-import Trash from './routes/Trash';
-import Login from './routes/Login';
-import Signup from './routes/Sign';
-import Modify from './routes/Modify';
-import OAuth2RedirectHandler from './routes/OAuth2RedirectHandeler';
-import { HashRouter, Route, BrowserRouter } from "react-router-dom";
-import Introduce from './routes/Introduce';
-=======
 import "./App.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -26,22 +12,12 @@ import OAuth2RedirectHandler from "./components/OAuth2RedirectHandeler";
 import { HashRouter, Route, BrowserRouter } from "react-router-dom";
 import Introduce from "./components/Introduce";
 import WriteUpdate from "./components/WriteUpdate";
->>>>>>> 79815462ce8030b6d4a0231ddceae70c6603fcb4
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-<<<<<<< HEAD
-        <Route path="/365" exact={true} component={Home}/>
-        <Route path="/write" component={Write}/>
-        <Route path="/list" component={List}/>
-        <Route path="/trash" component={Trash}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/introduce" component={Introduce} />
-        <Route path="/365/login/oauth_kakao" component={OAuth2RedirectHandler}></Route>
-=======
         <Route path="/365" exact={true} component={Home} />
         <Route exact path="/write" component={Write} />
         <Route path="/write/:id" component={WriteUpdate} />
@@ -54,7 +30,6 @@ function App() {
           path="/365/login/oauth_kakao"
           component={OAuth2RedirectHandler}
         ></Route>
->>>>>>> 79815462ce8030b6d4a0231ddceae70c6603fcb4
       </BrowserRouter>
     </div>
   );
